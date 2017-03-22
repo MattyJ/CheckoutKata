d22dff0503596cce892f904e21ba32d86263890e
Matthew Jordan <matthew.jordan@uk.fujitsu.com>
2017-03-22 12:22:17 +0000

    Scan product and initial tests

--- CheckoutKata/CheckoutKata.Tests/CheckoutTests.cs
+++ CheckoutKata/CheckoutKata.Tests/CheckoutTests.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 
 namespace CheckoutKata.Tests
 {
@@ -7,24 +7,52 @@ namespace CheckoutKata.Tests
     public class CheckOutTests
     {
         private ICheckout _checkout;
+        private List<Product> _products;
 
         [TestInitialize]
         public void Initialize()
         {
-            _checkout = new Checkout();
+            _products = new List<Product>
+            {
+                new Product
+                {
+                    Sku = "A",
+                    UnitPrice = 50
+                },
+                new Product
+                {
+                    Sku = "B",
+                    UnitPrice = 30
+                },
+                new Product
+                {
+                    Sku = "C",
+                    UnitPrice = 20
+                },
+                new Product
+                {
+                    Sku = "D",
+                    UnitPrice = 15
+                },
+            };
+
+            _checkout = new Checkout(_products);
         }
+
         [TestMethod]
-        public void Checkout_GetTotalPrice_ScanProductAReturnsTotalPriceOf50()
+        [DataRow("A", 50)]
+        [DataRow("B", 30)]
+        [DataRow("C", 20)]
+        [DataRow("D", 15)]
+        public void Checkout_GetTotalPrice_ScanOneProductOnlyReturnsCorrectTotalPrice(string sku, int totalPrice)
         {
             // Arrange
 
             // Act
-            _checkout.Scan("A");
+            _checkout.Scan(sku);
 
             // Assert
-            Assert.AreEqual(50, _checkout.GetTotalPrice());
-
-
+            Assert.AreEqual(totalPrice, _checkout.GetTotalPrice());
         }
     }
 }
